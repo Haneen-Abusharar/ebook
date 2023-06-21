a6302b2d6bdf4c5886379ad9f8d0a60a7f7a9d03
Haneen <haneensharar00@gmail.com>
2023-06-21 16:29:43 +0300

    added book scaffold and homepage logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_111143) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_21_130343) do
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.text "description"
+    t.integer "price"
+    t.boolean "availability", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
