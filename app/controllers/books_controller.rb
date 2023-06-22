6dd9add84b34aaf266ee64708ade46d43fff52c5
Haneen <haneensharar00@gmail.com>
2023-06-22 13:46:22 +0300

    new and edit page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show edit update destroy ]
+  before_action :set_book, only: [ :show, :edit, :update, :destroy ]
 
   # GET /books or /books.json
   def index
-    @books = Book.all
+    @books = Book.all.where(availability:true)
   end
 
   # GET /books/1 or /books/1.json
